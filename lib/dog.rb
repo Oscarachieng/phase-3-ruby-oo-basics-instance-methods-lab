bf6cb5db24cf75dad345f8bebedcb6d14814d970
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-18 17:32:22 +0300

    commit tree 193a582b53dafd85af6a30da3a2207a6e117ba5e

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+#Defining a Dog class
+ class Dog
+
+    #we define class method this way
+    def bark 
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting" 
+    end
+ end
+
+ my_dog = Dog.new
+ my_dog.bark
+ snoopy = Dog.new
+ snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+# define a class person
+class Person
+
+    def talk
+        puts "Hello World!"
+    end
+    def walk 
+        puts "The Person is walking" 
+    end
+end
+ada = Person.new
+ada.walk
+ada.talk
